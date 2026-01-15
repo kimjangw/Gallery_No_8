0c3ad332a794ff9f2c0af4e056ea50bbe74f7905
kimjangw <kimjangw95@gmail.com>
2026-01-15 18:00:56 +0900

    트랜지션 튐 제어
    -트랜지션 튐 제어

--- Assets/_Script/Transiton/TransitionController.cs
+++ Assets/_Script/Transiton/TransitionController.cs
@@ -31,6 +31,9 @@ public class TransitionController : MonoBehaviour
         hub.locked = true;
         linkedHub.locked = true;
 
+        // [ADD] 전환 구간: 카메라 보간 speed만 임시로 상승
+        cameraController.BeginTransitionBoost();
+
         loopManager.OnTransition(hub);
 
         cc.enabled = false;
@@ -44,11 +47,17 @@ public class TransitionController : MonoBehaviour
         cameraController.SnapToPlayerInstant();
 
         loopManager.ResetFixLine();
-
         loopManager.enemyController?.OnTransitionResetAll();
 
-        // [ADD] 전환 완료 시점에 ActionTrigger 잠금 해제(다음 구간에서 다시 발동 가능)
         if (actionTriggerToReset != null)
             actionTriggerToReset.UnlockForNextSection();
+
+        // [ADD] 다음 프레임부터는 원래 speed로 복귀 (즉시 끄면 같은 프레임 Lerp에 영향이 없을 수 있어 0프레임 딜레이)
+        Invoke(nameof(EndCameraBoost), 0f);
+    }
+
+    void EndCameraBoost()
+    {
+        cameraController.EndTransitionBoost();
     }
 }
